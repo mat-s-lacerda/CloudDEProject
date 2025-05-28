b41f1fb2fb6d94fb37f9d15bb4268df4cc970a0e
MSL <mat.s.lacerda@gmail.com>
2025-05-28 19:01:52 -0300

    [refactor] mudando api

--- meteorology/src/crf/main.py
+++ meteorology/src/crf/main.py
@@ -11,7 +11,8 @@ def entrypoint(request: Request) -> tuple[str, int, dict[str, str]]:
         functions-framework --target=entrypoint --port=8080
 
         curl -X POST http://localhost:8080 -H "Content-Type: application/json" -d '{"message":"Hello"}'
-    """
+        curl -X POST https://us-west1-analytics-meteorology-dev.cloudfunctions.net/crf-meteorology-dev  -H "Authorization: Bearer $(gcloud auth print-identity-token)" -H "Content-Type: application/json" -d '{"message":"Hello"}'
+        curl -o output.json "https://api.open-meteo.com/v1/forecast?latitude=-23.5475&longitude=-46.6361&hourly=temperature_2m,rain,wind_speed_10m&timezone=America%2FSao_Paulo&past_days=1"    """
     logger: logging.Logger = set_up_logger()
 
     logger.info("Hello from GCP Logging!")
