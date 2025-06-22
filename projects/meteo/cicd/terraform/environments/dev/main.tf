7398ebeea4842d29fdd0109ed802d4218c234f55
MSL <mat.s.lacerda@gmail.com>
2025-06-22 17:08:59 -0300

    [deploy] backend1

--- projects/meteo/cicd/terraform/environments/dev/main.tf
+++ projects/meteo/cicd/terraform/environments/dev/main.tf
@@ -2,8 +2,3 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
-
-resource "google_storage_bucket" "example" {
-  name     = "${var.project_id}-example-bucket"
-  location = var.region
-}
