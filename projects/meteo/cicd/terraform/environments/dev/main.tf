f29e1da0d89210d281898edafa35ec90e98c4d36
MSLacerda <mat.s.lacerda@gmail.com>
2025-07-01 21:17:46 -0300

    [terraform] major things

--- projects/meteo/cicd/terraform/environments/dev/main.tf
+++ projects/meteo/cicd/terraform/environments/dev/main.tf
@@ -1,26 +1,14 @@
-locals {
-  default_buckets = {
-    batch_data_intake = {
-      name          = "${var.project_id}-batch-data-intake"
-      location      = "${var.region}"
-      force_destroy = true
-      labels = {
-        env  = "${var.env}"
-        type = "raw"
-      }
-    }
-  }
-
-  resolved_buckets = length(var.buckets) > 0 ? var.buckets : local.default_buckets
+resource "google_project_service" "cloud_run" {
+  project = var.project_id
+  service = "run.googleapis.com"
 }
 
-module "buckets" {
-  for_each = local.resolved_buckets
+resource "google_project_service" "cloud_functions" {
+  project = var.project_id
+  service = "cloudfunctions.googleapis.com"
+}
 
-  source        = "../../modules/google_cloud_storage"
-  name          = each.value.name
-  location      = each.value.location
-  project_id    = var.project_id
-  force_destroy = each.value.force_destroy
-  labels        = each.value.labels
+resource "google_project_service" "artifact_registry" {
+  project = var.project_id
+  service = "artifactregistry.googleapis.com"
 }
