12240c332909d1a4fe5c5e162ba442804a178331
MSLacerda <mat.s.lacerda@gmail.com>
2025-08-31 17:24:54 -0300

    [infra] re-refactor

--- projects/meteo/infrastructure/core/buckets.tf
+++ projects/meteo/infrastructure/core/buckets.tf
@@ -1,36 +1,36 @@
-#locals {
-#  default_buckets = {
-#    batch_data_intake = {
-#      name          = "${var.project_id}-batch-data-intake"
-#      location      = "${var.region}"
-#      force_destroy = true
-#      labels = {
-#        env  = "${var.env}"
-#        type = "raw"
-#      }
-#    }
-#
-#    infra_data_safekeeping = {
-#      name          = "${var.project_id}-infra-data-safekeeping"
-#      location      = "${var.region}"
-#      force_destroy = true
-#      labels = {
-#        env  = "${var.env}"
-#        type = "infra"
-#      }
-#    }
-#  }
-#
-#  resolved_buckets = length(var.buckets) > 0 ? var.buckets : local.default_buckets
-#}
-#
-#module "buckets" {
-#  for_each = local.resolved_buckets
-#
-#  source        = "../modules/google_cloud_storage"
-#  name          = each.value.name
-#  location      = each.value.location
-#  project_id    = var.project_id
-#  force_destroy = each.value.force_destroy
-#  labels        = each.value.labels
-#}
+locals {
+  default_buckets = {
+    #batch_data_intake = {
+    #  name          = "${var.project_id}-batch-data-intake"
+    #  location      = "${var.region}"
+    #  force_destroy = true
+    #  labels = {
+    #    env  = "${var.env}"
+    #    type = "raw"
+    #  }
+    #}
+
+    infra_data_safekeeping = {
+      name          = "${var.project_id}-infra-data-safekeeping"
+      location      = "${var.region}"
+      force_destroy = true
+      labels = {
+        env  = "${var.env}"
+        type = "infra"
+      }
+    }
+  }
+
+  resolved_buckets = length(var.buckets) > 0 ? var.buckets : local.default_buckets
+}
+
+module "buckets" {
+  for_each = local.resolved_buckets
+
+  source        = "../modules/google_cloud_storage"
+  name          = each.value.name
+  location      = each.value.location
+  project_id    = var.project_id
+  force_destroy = each.value.force_destroy
+  labels        = each.value.labels
+}
--- projects/meteo/infrastructure/core/outputs.tf
+++ projects/meteo/infrastructure/core/outputs.tf
@@ -1,11 +1,11 @@
-#resource "google_secret_manager_secret" "bucket_name" {
-#  secret_id = "${var.env}-infra-data-safekeeping-bucket"
-#  replication {
-#    auto {}
-#  }
-#}
-#
-#resource "google_secret_manager_secret_version" "bucket_name" {
-#  secret      = google_secret_manager_secret.bucket_name.id
-#  secret_data = module.buckets["infra_data_safekeeping"].bucket_name
-#}
+resource "google_secret_manager_secret" "bucket_name" {
+  secret_id = "${var.env}-infra-data-safekeeping-bucket"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "bucket_name" {
+  secret      = google_secret_manager_secret.bucket_name.id
+  secret_data = module.buckets["infra_data_safekeeping"].bucket_name
+}
